e229c2b7a2323b991b54069597e09b8763cb2e23
sebastian medina <sebastianlopez_21@hotmail.com>
2023-04-21 11:57:09 -0400

    commit tree 36fc6eab7dc35dd7023a055acdac71b768229ded

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,18 @@
 class Bird < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    
+
+    def create 
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+    rescue ApplicationRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+        
+    end
+
+    private
+
+    def bird_params
+        params.permit(:name, :species, :likes)
+    end
 end
